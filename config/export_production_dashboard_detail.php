20f81e4feca1825732bc50b636026f4ced4a0888
Rizzking777 <rizki.zakaria14@gmail.com>
2026-05-28 09:06:14 +0700

    added dashboard production

---
+++ config/export_production_dashboard_detail.php
@@ -0,0 +1,415 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+/** @var mysqli $conn */
+
+require_once "function.php";
+
+/* PARAM */
+
+$section = $_GET['section'] ?? '';
+$type = $_GET['type'] ?? '';
+$ncvs = $_GET['ncvs'] ?? '';
+
+/* DEFAULT */
+
+$where = "1=1";
+$qtyFormula = "0";
+$mainComponentWhere = "";
+$ncvsWhere = "";
+
+/* NCVS FILTER */
+
+if (!empty($ncvs)) {
+
+    $ncvsWhere = "
+        AND ncvs = '$ncvs'
+    ";
+}
+
+/* CUTTING */
+
+if ($section == 'cutting') {
+
+    /* ALL COMPONENT */
+    $mainComponentWhere = "";
+
+    /* IN */
+    if ($type == 'in') {
+
+        $where = "
+            qty_cut_to_smsubcont > 0
+        ";
+
+        $qtyFormula = "
+            IFNULL(
+                qty_cut_to_smsubcont,
+                0
+            )
+        ";
+    }
+
+    /* OUT */ 
+    else if ($type == 'out') {
+        $where = "
+            qty_smsubcont_fr_cut > 0
+        ";
+
+        $qtyFormula = "
+            IFNULL(
+                qty_smsubcont_fr_cut,
+                0
+            )
+        ";
+    }
+
+    /* INVENTORY */
+    else if ($type == 'inventory') {
+        $where = "
+            (
+                IFNULL(
+                    qty_cut_to_smsubcont,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_fr_cut,
+                    0
+                )
+            ) > 0
+        ";
+
+        $qtyFormula = "
+            (
+                IFNULL(
+                    qty_cut_to_smsubcont,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_fr_cut,
+                    0
+                )
+            )
+
+        ";
+    }
+}
+
+/* PRE VENDOR */
+
+if ($section == 'pre_vendor') {
+
+    /* ALL COMPONENT */
+    $mainComponentWhere = "";
+
+    /* IN */
+    if ($type == 'in') {
+        $where = "
+            qty_smsubcont_fr_cut > 0
+        ";
+        $qtyFormula = "
+            IFNULL(
+                qty_smsubcont_fr_cut,
+                0
+            )
+        ";
+    }
+
+    /* OUT */
+    else if ($type == 'out') {
+        $where = "
+            qty_smsubcont_to_whsubcont > 0
+        ";
+        $qtyFormula = "
+            IFNULL(
+                qty_smsubcont_to_whsubcont,
+                0
+            )
+        ";
+    }
+
+    
+    /* INVENTORY */
+    else if ($type == 'inventory') {
+        $where = "
+            (
+                IFNULL(
+                    qty_smsubcont_fr_cut,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_to_whsubcont,
+                    0
+                )
+
+            ) > 0
+        ";
+
+        $qtyFormula = "
+            (
+                IFNULL(
+                    qty_smsubcont_fr_cut,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_to_whsubcont,
+                    0
+                )
+            )
+        ";
+    }
+}
+
+/* AFTER VENDOR */
+if ($section == 'after_vendor') {
+
+    /* MAIN COMPONENT ONLY */
+    $mainComponentWhere = "
+        AND is_main_komponen = 1
+    ";
+
+    /* IN */
+    if ($type == 'in') {
+        $where = "
+            qty_smsubcont_fr_whsubcont > 0
+        ";
+        $qtyFormula = "
+            IFNULL(
+                qty_smsubcont_fr_whsubcont,
+                0
+            )
+        ";
+    }
+
+    /* OUT */ 
+    else if ($type == 'out') {
+
+        $where = "
+            qty_smsubcont_to_prod > 0
+        ";
+        $qtyFormula = "
+            IFNULL(
+                qty_smsubcont_to_prod,
+                0
+            )
+        ";
+    }
+
+    /* INVENTORY */
+    else if ($type == 'inventory') {
+        $where = "
+            (
+                IFNULL(
+                    qty_smsubcont_fr_whsubcont,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_to_prod,
+                    0
+                )
+
+            ) > 0
+        ";
+        $qtyFormula = "
+            (
+                IFNULL(
+                    qty_smsubcont_fr_whsubcont,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_to_prod,
+                    0
+                )
+            )
+        ";
+    }
+}
+
+/* QUERY */
+
+$sql = "
+    SELECT
+        ncvs,
+        bucket,
+        style,
+        model,
+        po_code,
+        po_item,
+        nm_komponen_in AS component,
+        size,
+        SUM(
+            $qtyFormula
+        ) AS qty_total
+    FROM tbl_transaksi
+    WHERE
+        $where
+        $mainComponentWhere
+        $ncvsWhere
+    GROUP BY
+        ncvs,
+        bucket,
+        style,
+        model,
+        po_code,
+        po_item,
+        nm_komponen_in,
+        size
+    ORDER BY
+        ncvs ASC
+
+";
+
+$result = mysqli_query($conn, $sql);
+
+/* PREPARE */
+
+$sizes = [];
+$rows = [];
+
+/* LOOP */
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    $key = implode('|', [
+
+        $row['ncvs'],
+        $row['bucket'],
+        $row['style'],
+        $row['model'],
+        $row['po_code'],
+        $row['po_item'],
+        $row['component']
+
+    ]);
+
+    /* SIZE */
+
+    if (!in_array($row['size'], $sizes)) {
+
+        $sizes[] = $row['size'];
+    }
+
+    /* ROW INIT */
+
+    if (!isset($rows[$key])) {
+
+        $rows[$key] = [
+
+            'ncvs' => $row['ncvs'],
+            'bucket' => $row['bucket'],
+            'style' => $row['style'],
+            'model' => $row['model'],
+            'po' => $row['po_code'],
+            'po_item' => $row['po_item'],
+            'component' => $row['component'],
+            'sizes' => []
+
+        ];
+    }
+
+    /* SIZE QTY */
+
+    $rows[$key]['sizes'][$row['size']] =
+        (int)$row['qty_total'];
+}
+
+/* SORT SIZE */
+
+usort($sizes, function ($a, $b) {
+    $aNum = (int) preg_replace(
+        '/[^0-9]/',
+        '',
+        $a
+    );
+    $bNum = (int) preg_replace(
+        '/[^0-9]/',
+        '',
+        $b
+    );
+    if ($aNum == $bNum) {
+        $aIsT = str_contains($a, 'T');
+        $bIsT = str_contains($b, 'T');
+        if ($aIsT == $bIsT) {
+            return 0;
+        }
+        return $aIsT ? 1 : -1;
+    }
+    return $aNum <=> $bNum;
+});
+
+/* HEADER */
+
+header(
+    "Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+);
+
+header(
+    "Content-Disposition: attachment; filename=Dashboard_Detail.xlsx"
+);
+
+/* TABLE */
+
+echo "
+<table border='1'>
+
+<tr>
+    <th>NCVS</th>
+    <th>Bucket</th>
+    <th>Style</th>
+    <th>Model</th>
+    <th>PO</th>
+    <th>PO Item</th>
+    <th>Component</th>
+";
+
+/* SIZE HEADER */
+
+foreach ($sizes as $size) {
+
+    echo "
+        <th>$size</th>
+    ";
+}
+
+echo "
+    <th>Total</th>
+</tr>
+";
+
+/* BODY */
+
+foreach ($rows as $row) {
+
+    $total = 0;
+    echo "<tr>";
+    echo "<td>{$row['ncvs']}</td>";
+    echo "<td>{$row['bucket']}</td>";
+    echo "<td>{$row['style']}</td>";
+    echo "<td>{$row['model']}</td>";
+    echo "<td>{$row['po']}</td>";
+    echo "<td>{$row['po_item']}</td>";
+    echo "<td>{$row['component']}</td>";
+
+    foreach ($sizes as $size) {
+
+        $qty =
+            $row['sizes'][$size] ?? 0;
+
+        $total += $qty;
+
+        echo "<td>$qty</td>";
+    }
+
+    echo "<td><b>$total</b></td>";
+
+    echo "</tr>";
+}
+
+echo "</table>";
---
+++ config/get_production_dashboard_detail.php
@@ -0,0 +1,367 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+header('Content-Type: application/json');
+/** @var mysqli $conn */
+
+require_once "function.php";
+
+/* PARAM */
+$section = $_GET['section'] ?? '';
+$type = $_GET['type'] ?? '';
+$ncvs = $_GET['ncvs'] ?? '';
+
+/* CONDITION */
+$where = "";
+$qtyFormula = "0";
+$mainComponentWhere = "";
+$ncvsWhere = "";
+
+if (!empty($ncvs)) {
+
+    $ncvsWhere = "
+        AND ncvs = '$ncvs'
+    ";
+}
+
+/* CUTTING */
+if ($section == 'cutting') {
+
+    /* IN */
+    if ($type == 'in') {
+        $where = "
+            qty_cut_to_smsubcont > 0
+        ";
+        $qtyFormula = "
+            IFNULL(
+                qty_cut_to_smsubcont,
+                0
+            )
+        ";
+    }
+
+    /* OUT */ 
+    else if ($type == 'out') {
+
+        $where = "
+            qty_smsubcont_fr_cut > 0
+        ";
+        $qtyFormula = "
+            IFNULL(
+                qty_smsubcont_fr_cut,
+                0
+            )
+        ";
+    }
+
+    /* INVENTORY */
+    else if ($type == 'inventory') {
+
+        $where = "
+            (
+                IFNULL(
+                    qty_cut_to_smsubcont,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_fr_cut,
+                    0
+                )
+
+            ) > 0
+        ";
+
+        $qtyFormula = "
+            (
+                IFNULL(
+                    qty_cut_to_smsubcont,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_fr_cut,
+                    0
+                )
+            )
+        ";
+    }
+}
+
+/* PRE VENDOR */
+if ($section == 'pre_vendor') {
+
+    /* IN */
+    if ($type == 'in') {
+        $where = "
+            qty_smsubcont_fr_cut > 0
+        ";
+
+        $qtyFormula = "
+            IFNULL(
+                qty_smsubcont_fr_cut,
+                0
+            )
+        ";
+    }
+
+    /* OUT */
+    else if ($type == 'out') {
+
+        $where = "
+            qty_smsubcont_to_whsubcont > 0
+        ";
+
+        $qtyFormula = "
+
+            IFNULL(
+                qty_smsubcont_to_whsubcont,
+                0
+            )
+
+        ";
+    }
+    
+    /* INVENTORY */
+    else if ($type == 'inventory') {
+
+        $where = "
+            (
+                IFNULL(
+                    qty_smsubcont_fr_cut,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_to_whsubcont,
+                    0
+                )
+            ) > 0
+        ";
+
+        $qtyFormula = "
+            (
+                IFNULL(
+                    qty_smsubcont_fr_cut,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_to_whsubcont,
+                    0
+                )
+            )
+        ";
+    }
+}
+
+/* AFTER VENDOR */
+if ($section == 'after_vendor') {
+
+    /* MAIN COMPONENT ONLY */
+    $mainComponentWhere = "
+        AND is_main_komponen = 1
+    ";
+
+    /* IN */
+    if ($type == 'in') {
+
+        $where = "
+            qty_smsubcont_fr_whsubcont > 0
+        ";
+
+        $qtyFormula = "
+
+            IFNULL(
+                qty_smsubcont_fr_whsubcont,
+                0
+            )
+
+        ";
+    }
+
+    /* OUT */
+    else if ($type == 'out') {
+
+        $where = "
+            qty_smsubcont_to_prod > 0
+        ";
+
+        $qtyFormula = "
+
+            IFNULL(
+                qty_smsubcont_to_prod,
+                0
+            )
+
+        ";
+    }
+
+    /* INVENTORY */
+    else if ($type == 'inventory') {
+
+        $where = "
+            (
+                IFNULL(
+                    qty_smsubcont_fr_whsubcont,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_to_prod,
+                    0
+                )
+            ) > 0
+        ";
+
+        $qtyFormula = "
+            (
+                IFNULL(
+                    qty_smsubcont_fr_whsubcont,
+                    0
+                )
+                -
+                IFNULL(
+                    qty_smsubcont_to_prod,
+                    0
+                )
+            )
+        ";
+    }
+}
+
+/* QUERY */
+
+$sql = "
+    SELECT
+        ncvs,
+        bucket,
+        style,
+        model,
+        po_code,
+        po_item,
+        nm_komponen_in AS main_component,
+        size,
+        SUM(
+            $qtyFormula
+        ) AS qty_total
+
+    FROM tbl_transaksi
+
+    WHERE
+        $where
+        $mainComponentWhere
+        $ncvsWhere
+
+    GROUP BY
+
+        ncvs,
+        bucket,
+        style,
+        model,
+        po_code,
+        po_item,
+        nm_komponen_in,
+        size
+
+    ORDER BY
+        ncvs ASC
+
+";
+
+$result = mysqli_query($conn, $sql);
+
+/* PREPARE */
+
+$sizes = [];
+$rows = [];
+
+/* LOOP */
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    $key = implode('|', [
+
+        $row['ncvs'],
+        $row['bucket'],
+        $row['style'],
+        $row['model'],
+        $row['po_code'],
+        $row['po_item'],
+        $row['main_component']
+
+    ]);
+
+    /* SIZE */
+
+    if (!in_array($row['size'], $sizes)) {
+
+        $sizes[] = $row['size'];
+    }
+
+    /* ROW INIT */
+
+    if (!isset($rows[$key])) {
+
+        $rows[$key] = [
+
+            'ncvs' => $row['ncvs'],
+            'bucket' => $row['bucket'],
+            'style' => $row['style'],
+            'model' => $row['model'],
+            'po' => $row['po_code'],
+            'po_item' => $row['po_item'],
+            'component' =>
+            $row['main_component'],
+            'sizes' => []
+
+        ];
+    }
+
+    /* SIZE QTY */
+    $rows[$key]['sizes'][$row['size']] = (int)$row['qty_total'];
+}
+
+/* SORT SIZE */
+usort($sizes, function ($a, $b) {
+
+    /* NUMBER */
+    $aNum = (int) preg_replace(
+        '/[^0-9]/',
+        '',
+        $a
+    );
+
+    $bNum = (int) preg_replace(
+        '/[^0-9]/',
+        '',
+        $b
+    );
+
+    /* SAME NUMBER */
+    if ($aNum == $bNum) {
+
+        /* NORMAL FIRST */
+
+        $aIsT = str_contains($a, 'T');
+        $bIsT = str_contains($b, 'T');
+        if ($aIsT == $bIsT) {
+            return 0;
+        }
+        return $aIsT ? 1 : -1;
+    }
+
+    return $aNum <=> $bNum;
+});
+
+/* RESPONSE */
+
+$response = [
+    'sizes' => $sizes,
+    'rows' => array_values($rows)
+
+];
+
+echo json_encode($response);
